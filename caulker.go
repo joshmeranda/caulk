48b1bf4bd422b3f4b136030cdc927b656e5e30e2
joshmeranda <joshua.meranda@gmail.com>
2024-11-18 23:18:04 -0500

    find growable fields

--- caulker.go
+++ caulker.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"go/ast"
+	"go/parser"
 	"go/token"
 	"io"
 	"log/slog"
@@ -18,6 +20,11 @@ type Options struct {
 
 type Result struct{}
 
+type Growth map[string]struct {
+	GrownAtLines    []int
+	ShrinkedAtLines []int
+}
+
 type Caulker struct {
 	Options
 }
@@ -33,10 +40,69 @@ func NewCaulker(opts Options) *Caulker {
 }
 
 func (c *Caulker) Check(pkg *packages.Package) ([]Result, error) {
-	pkg.Fset.Iterate(func(file *token.File) bool {
-		fmt.Printf("checking file: %v\n", file.Name())
-		return true
-	})
+	for _, path := range pkg.GoFiles {
+		fset := token.NewFileSet()
+		file, err := parser.ParseFile(fset, path, nil, parser.SkipObjectResolution)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse file: %w", err)
+		}
+
+		// we don't care about bad declarations for parsing purposes
+		_, genDecl, _ := splitDeclarations(file.Decls)
+		for _, decl := range genDecl {
+			switch decl.Tok {
+			case token.TYPE:
+				spec, ok := decl.Specs[0].(*ast.TypeSpec)
+				if !ok {
+					panic(fmt.Sprintf("bug: expected *ast.TypeSpec but found %T", decl.Specs[0]))
+				}
+
+				switch t := spec.Type.(type) {
+				case *ast.StructType:
+					growableFields := findGrowableFields(t)
+					fmt.Printf("growable fields: %v\n", growableFields)
+				case *ast.Ident:
+					panic("non-struct types not yet supported")
+				}
+			case token.VAR:
+				panic("growable vars are not yet supported")
+			default:
+				continue
+			}
+		}
+	}
 
 	return nil, nil
+}
+
+func findGrowableFields(t *ast.StructType) []*ast.Field {
+	fields := make([]*ast.Field, 0, len(t.Fields.List))
+
+	for _, f := range t.Fields.List {
+		switch f.Type.(type) {
+		case *ast.ArrayType:
+			fields = append(fields, f)
+		case *ast.MapType:
+			panic("maps are not yet supported")
+		}
+	}
+
+	return fields
+}
+
+func splitDeclarations(decls []ast.Decl) (bad []*ast.BadDecl, gen []*ast.GenDecl, funcs []*ast.FuncDecl) {
+	for _, d := range decls {
+		switch d := d.(type) {
+		case *ast.GenDecl:
+			gen = append(gen, d)
+		case *ast.FuncDecl:
+			funcs = append(funcs, d)
+		case *ast.BadDecl:
+			bad = append(bad, d)
+		default:
+			panic(fmt.Sprintf("bug: unknown declaration type: %T", d))
+		}
+	}
+
+	return
 }
